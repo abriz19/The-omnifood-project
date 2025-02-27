e7864fdbecadbbf359eeb53c0894d313648a6558
Abreham Dessu <abrizdessu@gmail.com>
2025-02-27 00:07:20 -0800

    feat: implement a sticky navigation

--- script.js
+++ script.js
@@ -35,3 +35,25 @@ allLinks.forEach(function (link) {
     }
   });
 });
+
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const observer = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    } else {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // Consider the user view port as the root element
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+observer.observe(sectionHeroEl);
